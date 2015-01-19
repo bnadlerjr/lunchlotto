6918e4be1d3dfe3e0539d0e78a14201a76c82992
Bob Nadler <bnadlerjr@gmail.com>
2015-01-19 15:58:58 -0500

    Leiningen alias for code linting.
    Runs lein ancient, kibit and eastwood.

--- project.clj
+++ project.clj
@@ -30,4 +30,5 @@
             [lein-ancient "0.5.5"]
             [lein-kibit "0.0.8"]
             [jonase/eastwood "0.2.1"]
-            [lein-bikeshed "0.2.0"]])
+            [lein-bikeshed "0.2.0"]]
+  :aliases {"lint" ["do" ["ancient"] ["kibit"] ["eastwood"]]})
