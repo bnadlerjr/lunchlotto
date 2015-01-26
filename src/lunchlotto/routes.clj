a8a1755525d457532771a86dfe06a4fd8d339e65
Bob Nadler <bnadlerjr@gmail.com>
2015-01-26 10:10:29 -0500

    Separate anonymous and secure routes.

--- src/lunchlotto/routes.clj
+++ src/lunchlotto/routes.clj
@@ -8,28 +8,32 @@
             [cemerick.friend :as friend]
             (cemerick.friend [workflows :as workflows])))
 
-(def application-routes
-  (friend/authenticate
+(def anonymous
+  (routes
+    (GET "/register" [] auth/show-registration-page)
+    (POST "/register" {params :params} (auth/register-user params))
+    (PUT "/register" [] auth/update-confirmation-token)
+    (GET "/confirm" [] auth/show-confirmation-page)
+    (POST "/confirm" {params :params} (auth/confirm-user params))
+    (GET "/login" [] auth/show-login-page)))
+
+(def secure-user
+  (friend/wrap-authorize
     (routes
       (GET "/" [] common/home)
-
-      (GET "/register" [] auth/show-registration-page)
-      (POST "/register" {params :params} (auth/register-user params))
-      (PUT "/register" [] auth/update-confirmation-token)
-
-      (GET "/confirm" [] auth/show-confirmation-page)
-      (POST "/confirm" {params :params} (auth/confirm-user params))
-
-      (GET "/login" [] auth/show-login-page)
-
       (GET "/lunches/upcoming" [] lunches/show-upcoming)
       (GET "/lunches/pending" [] lunches/show-pending)
       (GET "/lunches/recommended" [] lunches/show-recommended)
+      (GET "/settings" [] settings/show-settings))
+    #{:lunchlotto.auth.handlers/user}))
 
-      (GET "/settings" [] settings/show-settings)
+(def application-routes
+  (friend/authenticate
+    (routes
+      anonymous
+      secure-user
       (not-found "Page not found."))
-    {:allow-anon true
-     :login-uri "/login"
+    {:login-uri "/login"
      :login-failure-handler auth/failed-login
      :default-landing-uri "/"
      :credential-fn auth/authenticate
