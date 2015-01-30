b81d95e079574737521fdb38eb13b6158a275074
Bob Nadler <bnadlerjr@gmail.com>
2015-01-29 20:04:46 -0500

    Extract small helper for making forms.

--- src/lunchlotto/auth/views.clj
+++ src/lunchlotto/auth/views.clj
@@ -12,6 +12,15 @@
    (func (merge {:id name :class "form-control" :value value} opts) name)
    (when error [:span.help-block error])])
 
+(defn- make-form
+  [method url button-text & body]
+  (f/form-to
+    [method url]
+    (ring/anti-forgery-field)
+    body
+    [:div.form-group
+     (f/submit-button {:class "btn btn-primary"} button-text)]))
+
 (defn register-page
   "User registration page."
   [params]
@@ -20,16 +29,11 @@
                   {:button-text "Register" :method :post})]
     (layout
       [:h2 "Register New User"]
-      (f/form-to
-        [(:method options) "/register"]
-        (ring/anti-forgery-field)
-        (make-field f/text-field "email" {:label "Cyrus Email"
-                                          :error (get-in params [:errors :email])
-                                          :value (:email params)
-                                          :opts {:required true :autofocus true}})
-
-        [:div.form-group
-         (f/submit-button {:class "btn btn-primary"} (:button-text options))]))))
+      (make-form (:method options) "/register" (:button-text options)
+                 (make-field f/text-field "email" {:label "Cyrus Email"
+                                                   :error (get-in params [:errors :email])
+                                                   :value (:email params)
+                                                   :opts {:required true :autofocus true}})))))
 
 (defn confirmation-page
   "Allows user to complete their registration by providing a password and
@@ -38,9 +42,7 @@
   (layout
     [:h2 "Finish Registration"]
     [:p "Thanks for confirming your email address! Follow the steps below to finish your registration."]
-    (f/form-to [:post "/confirm"]
-               (ring/anti-forgery-field)
-
+    (make-form :post "/confirm" "Finish Registration"
                (make-field f/password-field "password" {:label "Password"
                                                         :error (get-in params [:errors :password])
                                                         :value (:password params)
@@ -57,10 +59,7 @@
 
                (f/hidden-field {:id "confirmation_token" :value (:confirmation_token params)} "confirmation_token")
                (f/hidden-field {:id "latitude" :value (:latitude params)} "latitude")
-               (f/hidden-field {:id "longitude" :value (:longitude params)} "longitude")
-
-               [:div.form-group
-                (f/submit-button {:class "btn btn-primary"} "Finish Registration")])))
+               (f/hidden-field {:id "longitude" :value (:longitude params)} "longitude"))))
 
 (defn login-page
   "Allows user to login."
@@ -69,12 +68,8 @@
     (when-not (nil? flash) [:div {:class "alert alert-info"} flash])
     [:h2 "Login"]
     [:p "New here? " (e/link-to "/register" "Register") " for an account."]
-    (f/form-to [:post "/login"]
-               (ring/anti-forgery-field)
+    (make-form :post "/login" "Login"
                (make-field f/text-field "username" {:label "Email"
-                                                    :opts {:required true :autofocus true}})
+                                                    :opts  {:required true :autofocus true}})
 
-               (make-field f/password-field "password" {:label "Password"})
-
-               [:div.form-group
-                (f/submit-button {:class "btn btn-primary"} "Login")])))
+               (make-field f/password-field "password" {:label "Password"}))))
