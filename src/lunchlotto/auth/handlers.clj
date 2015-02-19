58831daa9d1a37d145d666c1b6c7977472a76a44
Bob Nadler <bnadlerjr@gmail.com>
2015-02-19 08:38:50 -0500

    Prune user auth hash fields.

--- src/lunchlotto/auth/handlers.clj
+++ src/lunchlotto/auth/handlers.clj
@@ -94,10 +94,7 @@
   "Authenticates a user with the given username (email) and password."
   [{:keys [username password]}]
   (when-let [user (models/authenticate-user db username password)]
-    (assoc
-      (select-keys user [:id :email :location :latitude :longitude])
-      :username username
-      :roles #{::user})))
+    (assoc (select-keys user [:id]) :username username :roles #{::user})))
 
 (defn failed-login
   [_]
