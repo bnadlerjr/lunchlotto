d4a9e641a89913406a4a71a25c9bf6ee0aa4ec4a
Bob Nadler <bnadlerjr@gmail.com>
2015-02-19 23:12:15 -0500

    Tests for settings.handlers.delete-user.

--- test/lunchlotto/settings/handlers_test.clj
+++ test/lunchlotto/settings/handlers_test.clj
@@ -4,9 +4,17 @@
   (:require [lunchlotto.settings.handlers :as handlers]
             [lunchlotto.settings.models :as models]))
 
-(deftest settings-page
+(deftest show-settings
   (testing "successfully render settings page"
     (with-redefs [models/find-user-by-id (fn [_ _])]
       (let [{:keys [:status :body]} (handlers/show-settings {})]
         (is (= 200 status))
         (is (.contains body "Settings"))))))
+
+(deftest delete-user
+  (testing "successfully delete account"
+    (with-redefs [models/delete-user (fn [_ _])]
+      (let [{:keys [:status :headers :flash]} (handlers/delete-user {})]
+        (is (= 302 status))
+        (is (= {"Location" "/"} headers))
+        (is (= "Your account has been deleted." flash))))))
