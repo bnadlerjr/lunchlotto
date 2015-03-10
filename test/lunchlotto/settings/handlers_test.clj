9a3d34020c0e978902adc51574091c9e5801a02d
Bob Nadler <bnadlerjr@gmail.com>
2015-03-10 15:33:22 -0400

    Stub out database call in settings handler test.

--- test/lunchlotto/settings/handlers_test.clj
+++ test/lunchlotto/settings/handlers_test.clj
@@ -15,21 +15,22 @@
         (is (= {:user {:id 1}} (.params response)))))))
 
 (deftest update-settings
-  (testing "successfully update settings"
-    (with-redefs [models/update-settings (fn [_ _] true)
-                  val/validate-settings (fn [_ _] [true {}])]
-      (let [{:keys [:status :headers :flash]} (handlers/update-settings {})]
-        (is (= 302 status))
-        (is (= "/settings" (get headers "Location")))
-        (is (= "Your settings have been updated." flash)))))
+  (with-redefs [models/find-user-by-id (fn [_ _] true)]
+    (testing "successfully update settings"
+      (with-redefs [models/update-settings (fn [_ _] true)
+                    val/validate-settings (fn [_ _] [true {}])]
+        (let [{:keys [:status :headers :flash]} (handlers/update-settings {})]
+          (is (= 302 status))
+          (is (= "/settings" (get headers "Location")))
+          (is (= "Your settings have been updated." flash)))))
 
-  (testing "cannot update because of validation error"
-    (with-redefs [models/update-settings (fn [_ _] true)
-                  val/validate-settings (fn [_ _] [false {}])]
-      (let [response (handlers/update-settings {})]
-        (is (= 400 (.status response)))
-        (is (= "settings/show.html" (.template response)))
-        (is (= {:user {:email nil}} (.params response)))))))
+    (testing "cannot update because of validation error"
+      (with-redefs [models/update-settings (fn [_ _] true)
+                    val/validate-settings (fn [_ _] [false {}])]
+        (let [response (handlers/update-settings {})]
+          (is (= 400 (.status response)))
+          (is (= "settings/show.html" (.template response)))
+          (is (= {:user {:email nil}} (.params response))))))))
 
 (deftest delete-user
   (testing "successfully delete account"
